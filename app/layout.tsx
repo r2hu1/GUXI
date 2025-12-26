23fe7aaf86dfda15b6bd2d40d8aa1553314426cc
r2hu1 <newasram86@gmail.com>
2025-12-26 19:48:34 +0530

    feat: init hono w/ nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Guxi",
+  description: "Tweet your GitHub launches automatically using AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+  return <p>Welcome to Guxi!</p>;
+}
